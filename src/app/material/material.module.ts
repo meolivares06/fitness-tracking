9f74d082ee8e60b1b668de5d8f98165d29e1b088
Mario Olivares <mario.olivares@datys.cu>
2020-03-19 12:31:35 -0400

    Signup Form Variante.  - MatCard as container inside section  - Appearance outline

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -7,11 +7,27 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-
+import { MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
-  exports: [MatFormFieldModule, MatInputModule, MatCheckboxModule, MatButtonModule, MatDatepickerModule, MatNativeDateModule],
-  imports: [MatFormFieldModule, MatInputModule, MatCheckboxModule, MatButtonModule, MatDatepickerModule, MatNativeDateModule]
+  exports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCardModule
+  ],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCardModule
+  ]
 })
 export class MaterialModule {}
